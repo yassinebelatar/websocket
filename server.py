e6f3fb1a734b6c670dac4d8b786c70696eced74b
yassinebelatar <76491500+yassinebelatar@users.noreply.github.com>
2023-03-07 19:58:21 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,18 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
+from simple_websocket import WebSocket
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, websocket=WebSocket)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
-
-@socketio.on('message')
-def handle_message(data):
-    print('Received message:', data)
-    socketio.send('Server received message: ' + data)
+@socketio.on('my_event')
+def test_message(message):
+    emit('my_response', {'data': message['data']})
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port=5000)
